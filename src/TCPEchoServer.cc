b57a14389e404fa8d15df9b39cc3727ae53c55f8
patriciohg <patricio.hinojosa@alumnos.uv.cl>
2018-12-10 21:46:36 -0300

    avanzando con la lectura del Json

--- src/TCPEchoServer.cc
+++ src/TCPEchoServer.cc
@@ -1,15 +1,16 @@
 #include "YASL.h"      // For Socket, ServerSocket, and SocketException
-#include "checkArgs.h"
 #include <iostream>    // For cerr and cout
 #include <cstdlib>     // For atoi()
-#include "Json.hpp"
+#include "json.hpp"
 #include <string>
 #include "regex.h"
-
+#include <fstream>
 
 const uint32_t RCVBUFSIZE = 32;    // Size of receive buffer
-
+using json = nlohmann::json;
 // TCP client handling function
+std::string root_dir="", notFoundFile="";
+void leer(std::string pagina);
 void HandleTCPClient(TCPSocket *sock) {
 	std::cout << "Handling client ";
 	try {
@@ -25,13 +26,14 @@ void HandleTCPClient(TCPSocket *sock) {
 	std::cout << std::endl;
 
 	// Send received string and receive again until the end of transmission
-	char echoBuffer[RCVBUFSIZE];
-	
+	char echoBuffer[RCVBUFSIZE];	
 	int cont= 0,control =1,i=0;
 	std::string pagina="";
-	uint32_t recvMsgSize;
 
+	uint32_t recvMsgSize;
+	
 	while ((recvMsgSize = sock->recv(echoBuffer, RCVBUFSIZE)) > 0) { // Zero means
+		//obtencion del GET enviado por el cliente
 		i =0;
 		while(control == 1){
 			i++;
@@ -41,27 +43,52 @@ void HandleTCPClient(TCPSocket *sock) {
 				while(1){
 					pagina+=echoBuffer[cont];
 					if(echoBuffer[cont]==' '){
-						std::cout << pagina<<std::endl;
+						std::cout << "1"+pagina+"1"<<std::endl;
 						control = 0;
+						leer(pagina);
 						break;
 					}	
 					cont++;
 				}				
 			}
 		}
+		//leer la pagina html pedida por el cliente
+
+		
+		
 		sock->send(echoBuffer, recvMsgSize);	
 	}
 	delete sock;
 } 
+json leer_json(){
+	json j;
+    std::ifstream ifs("bin/config.json");
+    j = json::parse(ifs);
+    return j;
+}
+void leer(std::string pagina){
+		std::ifstream bafer("WWW-data/"+pagina);
+		std::string a;
+		if(bafer.good()){
+			std::cout <<"existe"<< std::endl;
+			while(!bafer.eof()){
+				bafer>>a;	
+			}
+		}else{
+			std::cout <<"no existe"<< std::endl;
+		}	
+
+}
 int main(int argc, char *argv[]) {
-	
-	checkArgs* argumentos = new checkArgs(argc, argv);
-	
+	json cfg = leer_json();
 	uint16_t echoServPort;   
-	echoServPort  = argumentos->getArgs().PORT;
+	std::string ip = cfg["ip"];
+	echoServPort  = cfg["puerto"];
+	root_dir = cfg["root_dir"];
+	notFoundFile = cfg["notFoundFile"];
 
 	try {
-		TCPServerSocket servSock(echoServPort);     // Server Socket object
+		TCPServerSocket servSock(ip,echoServPort);     // Server Socket object
 		for (;;) {   // Run forever
 			HandleTCPClient(servSock.accept());
 			
